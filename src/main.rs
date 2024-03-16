9a64ca99d28aa675313e11ecb7a45e9e2b6c1628
ahsan-javaid <ahsan786cs@gmail.com>
2024-03-16 12:59:25 +0500

    devop: tcp listen for ping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// Uncomment this block to pass the first stage
+use std::{ io::Write, net::TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
+
+    // Uncomment this block to pass the first stage
+    
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let result = stream.write(b"+PONG\r\n");
+                
+                match result {
+                    Ok(bytes) => println!("bytes written: {bytes}"),
+                    Err(e) => println!("Error writing to stream, {:?}", e)
+                }
+                
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
