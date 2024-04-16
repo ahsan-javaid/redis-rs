e58bc90d7319cbc7447c20f816d1c5d9faaa0c31
ahsan-javaid <ahsan786cs@gmail.com>
2024-04-16 11:43:35 +0500

    devop: parse simple string

--- src/libs/stream_handler.rs
+++ src/libs/stream_handler.rs
@@ -5,6 +5,20 @@ use std:: {
   str:: FromStr
 };
 
+pub enum Value {
+  SimpleString(String),
+  BulkSttring(String),
+  Array(Vec<Value>)
+}
+impl Value {
+  pub fn serialize(self) -> String {
+    match self {
+      Value::SimpleString(s) => format!("+{}\r\n", s),
+      Value::BulkSttring(s) => format!("${}\r\n{}\r\n", s.chars().count(), s),
+      _ => panic!("unsupported value for serialize")
+    }
+  }
+}
 pub struct StreamHandler<'a> {
   reader: BufReader<TcpStream>,
   writer: BufWriter<&'a TcpStream>
@@ -21,17 +35,66 @@ impl<'a> StreamHandler <'a> {
 
   pub fn handle(&mut self) {
     loop {
+      let values: Vec<String> = Vec::new();
+
       let input: String = self._read().ok().unwrap();
      
       if input.len() == 0 {
         break;
       }
+      
 
       for line in input.lines() {
         if line.trim().len() > 0 {
+
+          // parser start 
+          match line.chars().next().unwrap() {
+            '+' => {
+              // parse simple strings
+
+              let mut temp: String = String::from(line.to_string());
+
+              // remove + sign
+              temp.remove(0);
+
+              let parsed_value = Value::SimpleString(temp);
+              // write back result
+            }
+            '*' => {
+              // parse arrays
+
+            }
+            '$' => {
+              // parse bulk strings
+
+            }
+            _ => {
+              // panic
+            }
+          }
+          // parser end
+
           let output: RedisCmd = RedisCmd::from_str(line.trim()).ok().unwrap();
+
           match output {
             RedisCmd::Ping => self._write(output),
+            RedisCmd::Echo => {
+              //  "*2\r\n$4\r\necho\r\n$9\r\nraspberry\r\n"
+              // process echo 
+              loop {
+                let input: String = self._read().ok().unwrap();
+     
+                if input.len() == 0 {
+                  break;
+                }
+                for l in input.lines() {
+
+                  let resp = format!("$3\r\n{l}\r\n");
+                  self.writer.write_all(resp.as_bytes()).unwrap();
+                  self.writer.flush().unwrap();
+                }
+              }
+            }
             RedisCmd::Unsupported => {}
           }
         }
