0bb6fc0a57405d5ea6d7a9156b3c6e5112e6f937
ahsan-javaid <ahsan786cs@gmail.com>
2024-07-16 19:56:58 +0500

    devop: update port parser

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -5,3 +5,15 @@ pub fn parse_replica(args: &[String]) -> Option<(String, u16)> {
     (args.get(i+1).unwrap().clone(), args.get(i+2).unwrap().parse::<u16>().unwrap())
   })
 }
+
+// parse port
+// if let Some(index) = args.iter().position(|x| x.contains("port")) {
+//   if index < args.len() - 1 {
+//       port_num = args[index + 1].clone();
+//   }
+// }
+pub fn parse_port(args: &[String]) -> Option<String> {
+  args.iter().position(|item| item == "--port").map(|i| {
+    args.get(i+1).unwrap().clone()
+  })
+}
