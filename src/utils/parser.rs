15a43f11f8e164cd4ae0927684cff0de6d302b46
ahsan-javaid <ahsan786cs@gmail.com>
2024-07-25 19:39:58 +0500

    devop: update parsing methods

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,13 +1,13 @@
 // Return tuple of host and port 
-pub fn parse_replica(args: &[String]) -> Option<(String, u16)> {
+pub fn parse_multi_arg(args: &[String], arg_name: &str) -> Option<(String, u16)> {
   //format: --replicaof host port
-  args.iter().position(|item| item == "--replicaof").map(|i| {
+  args.iter().position(|item| item == arg_name).map(|i| {
     (args.get(i+1).unwrap().clone(), args.get(i+2).unwrap().parse::<u16>().unwrap())
   })
 }
 
-pub fn parse_port(args: &[String]) -> Option<String> {
-  args.iter().position(|item| item == "--port").map(|i| {
+pub fn parse_single_arg(args: &[String], arg_name: &str) -> Option<String> {
+  args.iter().position(|item| item == arg_name).map(|i| {
     args.get(i+1).unwrap().clone()
   })
 }
